d6574f6bfbd77151db0777999cac03cd298a2e66
adithyatns <adithya.tns@gmail.com>
2026-03-05 23:27:24 +0530

    updating finder-app for cross compilation

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -32,9 +32,9 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
-if [ $assignment != 'assignment1' ]
+if [ $assignment != 'assignment4' ]
 then
 	mkdir -p "$WRITEDIR"
 
@@ -54,10 +54,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/assignment4-result.txt" "$WRITESTR"
+	writer "$WRITEDIR/assignment4-result.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp
